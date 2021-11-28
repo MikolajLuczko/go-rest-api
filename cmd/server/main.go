e96fa9fb0bbf7fa600b081d2baedaa59eef2a231
Mikolaj Luczko <mikolaj.luczko@gmail.com>
2021-11-28 18:32:42 +0100

    Add basic app structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// the App struct will contain things like pointers to db connections CHANGE THIS COMMENT LATER
+type App struct{}
+
+// sets up the application
+func (app *App) Run() error {
+	fmt.Println("Setting up the API")
+	return nil
+}
+
 func main() {
 	fmt.Println("Welcome to this GO REST API")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up the REST API")
+		fmt.Println(err)
+	}
 }
